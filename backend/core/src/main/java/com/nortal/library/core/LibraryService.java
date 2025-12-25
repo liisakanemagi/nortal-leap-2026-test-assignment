77e7e4cdf4a398da1df1062cb92dca667a7f2112
liisakanemagi <liisa.kanemagi@gmail.com>
2025-12-25 17:08:36 +0200

    Check if book is already loaned

--- backend/core/src/main/java/com/nortal/library/core/LibraryService.java
+++ backend/core/src/main/java/com/nortal/library/core/LibraryService.java
@@ -35,13 +35,14 @@ public class LibraryService {
         if (!canMemberBorrow(memberId)) {
             return Result.failure("BORROW_LIMIT");
         }
-
         Book entity = book.get();
+        if (entity.getLoanedTo() != null) {
+            return Result.failure("BOOK_ALREADY_LOANED");
+        }
         entity.setLoanedTo(memberId);
         entity.setDueDate(LocalDate.now().plusDays(DEFAULT_LOAN_DAYS));
         bookRepository.save(entity);
         return Result.success();
-        //lisa if konditsioon kontrollimaks, kas raamat on juba laenutatud
         // lisa jarjekorra kontroll
     }
 
